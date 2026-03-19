9c63f98fdc925f4f266c582edc85b734438350a5
Philippe Mathieu <philippe.mathieu@univ-lille.fr>
2026-03-19 09:53:17 +0100

    bug obligation de surcouper

--- mapp_card.py
+++ mapp_card.py
@@ -116,11 +116,18 @@ class Player(ABC):
         # sinon, est-ce que j'ai de l'atout ?
         elif trumps_in_hand and rules.kappa: # Obligation de couper
             res = trumps_in_hand
-            # Obligation de surcouper (AC : ici on ne s'intéresse pas au partenaire ?)
-            if rules.mu and trumps_in_trick:
-                if highest_ref is not None:
-                    higher_t = [c for c in trumps_in_hand if c.value > highest_ref.value]
-                    if higher_t: res = higher_t
+            # Obligation de surcouper
+            # Vérifier si c'est un adversaire (et non le partenaire) qui détient le meilleur atout
+            cutter = max(
+                [(p, c) for p, c in trick if c.suit == trump_suit],
+                key=lambda x: x[1].value
+            )
+            adversary_is_winning = cutter[0].team != self.team
+
+            if rules.mu and trumps_in_trick and adversary_is_winning:
+                higher_t = [c for c in trumps_in_hand if c.value > highest_ref.value]
+                if higher_t: res = higher_t
+
             return res
         # si on n'a pas appliqué de filtre
         return self.hand[:] # copie de self.hand
